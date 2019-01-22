616f887568670370f0b61b3ef4db9ce46bd1312e
geoffwhittington <geoff@sdelements.com>
2019-01-22 14:36:13 -0500

    create groups and assign them to BU's and projects

--- import_projects.py
+++ import_projects.py
@@ -7,6 +7,24 @@ import os
 import re
 import requests
 
+import logging
+import contextlib
+try:
+    from http.client import HTTPConnection # py3
+except ImportError:
+    from httplib import HTTPConnection # py2
+
+def debug_requests_on():
+    '''Switches on logging of the requests module.'''
+    HTTPConnection.debuglevel = 1
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+debug_requests_on()
 
 def get_headers(args):
     return {
@@ -27,8 +45,61 @@ def transform_value(value, data):
     return Template(value).substitute(data)
 
 
-def get_or_create_bu(args, bu_name, validate_cert):
-    
+def get_x_role(args, role_type, role_name, validate_cert):
+
+    params = {}
+    request_url = "https://{0}/api/v2/{1}-roles/".format(args.sde_host, role_type)
+    response = requests.get(request_url, params=params, headers=get_headers(args), verify=validate_cert)
+
+    if not response.ok:
+        # Request did not go through because of an error, print the error
+        print "{r.status_code} {r.reason}: {r.text}".format(r=response)
+        exit()
+
+    roles = response.json()['results']
+    for role in roles:
+        if role['name'] == role_name:
+            return role['id']
+
+    raise Exception("Missing {0} role: {1}".format(role_type, role_name))
+
+def get_or_create_group(args, group_name, role_name, validate_cert):
+
+    params = {}
+    request_url = "https://{0}/api/v2/groups/".format(args.sde_host)
+    response = requests.get(request_url, params=params, headers=get_headers(args), verify=validate_cert)
+
+    if not response.ok:
+        # Request did not go through because of an error, print the error
+        print "{r.status_code} {r.reason}: {r.text}".format(r=response)
+        exit()
+
+    groups = response.json()['results']
+    for group in groups:
+        if group['name'] == group_name:
+            return group['id']
+
+    body = {
+       'name': group_name,
+       'role': get_x_role(args, 'global', role_name, validate_cert)
+    }
+    response = requests.post(request_url, json=body, headers=get_headers(args), verify=validate_cert)
+    if not response.ok:
+        # Request did not go through because of an error, print the error
+        print "{r.status_code} {r.reason}: {r.text}".format(r=response)
+        exit()
+
+    group = response.json()
+    return group['id']
+
+def get_or_create_bu(args, bu_name, groups, validate_cert):
+
+    bu_groups = []
+    for g in groups.keys():
+        bu_groups.append({
+            'id': get_or_create_group(args, g, groups[g], validate_cert),
+        })
+
     # Send the HTTP request to the APIa
     params = {
        'name': bu_name
@@ -49,7 +120,8 @@ def get_or_create_bu(args, bu_name, validate_cert):
 
     else:
         request_body = {
-            "name": bu_name
+            "name": bu_name,
+            "groups": bu_groups
         }
 
         # Send the HTTP request to the API
@@ -105,7 +177,15 @@ def get_or_create_application(args, bu_id, app_name, attrs, validate_cert):
     return app
 
 
-def create_project(args, application_id, project_name, profile_id, attrs, validate_cert):
+def create_project(args, application_id, project_name, profile_id, groups, attrs, validate_cert):
+
+    proj_groups = []
+    for g in groups.keys():
+        proj_groups.append({
+            'id': get_or_create_group(args, g, "User", validate_cert),
+            'role': get_x_role(args, 'project', groups[g], validate_cert)
+        })
+
     request_body = {
         "application": application_id,  # The ID of the application the project should be created under.
         "name": project_name,  # The name of the new project.
@@ -113,6 +193,7 @@ def create_project(args, application_id, project_name, profile_id, attrs, valida
         # The ID of the desired profile for the project.
         # This field is not required
         "profile": profile_id,
+        "groups": proj_groups,
         "custom_attributes": attrs
     }
 
@@ -131,22 +212,31 @@ def create_project(args, application_id, project_name, profile_id, attrs, valida
 
 def import_project(args, row, mapping, validate_cert):
 
-    bu_name = transform_value(mapping['business_unit'], row)
-    app_name = transform_value(mapping['application_name'], row)
-    project_name = transform_value(mapping['project_name'], row)
+    bu_name = transform_value(mapping['business_unit']['name'], row)
+    app_name = transform_value(mapping['application']['name'], row)
+    project_name = transform_value(mapping['project']['name'], row)
+    project_profile =  transform_value(mapping['project']['profile'], row)
+ 
+    bu_groups = {}
+    for g in mapping['business_unit']['groups']:
+        bu_groups[transform_value(g['name'], row)] = transform_value(g['role'], row)
+
+    proj_groups = {}
+    for g in mapping['project']['groups']:
+        proj_groups[transform_value(g['name'], row)] = transform_value(g['role'], row)
 
     project_attrs = {}
-    for a in mapping['project_attrs'].keys():
-        project_attrs[a] = transform_value(mapping['project_attrs'][a], row)
+    for a in mapping['project']['attributes'].keys():
+        project_attrs[a] = transform_value(mapping['project']['attributes'][a], row)
 
     app_attrs = {}
-    for a in mapping['application_attrs'].keys():
-        app_attrs[a] = transform_value(mapping['application_attrs'][a], row)
+    for a in mapping['application']['attributes'].keys():
+        app_attrs[a] = transform_value(mapping['application']['attributes'][a], row)
 
 
-    bu = get_or_create_bu(args, bu_name, validate_cert)
+    bu = get_or_create_bu(args, bu_name, bu_groups, validate_cert)
     app = get_or_create_application(args, bu['id'], app_name, app_attrs, validate_cert)
-    return create_project(args, app['id'], project_name, 'P1', project_attrs, validate_cert)
+    return create_project(args, app['id'], project_name, project_profile, proj_groups, project_attrs, validate_cert)
 
 def main(args):
     count =0 
