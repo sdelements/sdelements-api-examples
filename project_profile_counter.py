2b57360fdb55d9464f47d2fb1939395060ccacfb
Athi Rajkumar <arajkumar@securitycompass.com>
2019-05-27 12:11:20 -0400

    Added some print statements for debugging purposes

--- project_profile_counter.py
+++ project_profile_counter.py
@@ -5,13 +5,16 @@ import requests
 
 
 SERVER = os.environ['SDE_SERVER']         # e.g. https://cd.sdelements.com
-API_TOKEN = os.environ['SDE_API_TOKEN']   # e.g. f258c47557a3f98f55d4fbd0cb9d8354c86fbb52
+API_TOKEN = os.environ['SDE_API_TOKEN']   # e.g. f258c47557a3f98f55d4fbd0icb9d8354c86fbb52
 
 PROJECTS_URL = '{server}/api/v2/projects/?page_size=100'.format(server=SERVER)
 REQUESTS_HEADER = {
     'Authorization': "Token " + API_TOKEN,  # Authorization header + generated API TOKEN
     'Accept': "application/json"
 }
+
+print("Fetching projects from: {}".format(PROJECTS_URL))
+print("Using API Token: {}...".format(API_TOKEN[:5]))
 
 url = PROJECTS_URL
 profiles = []
@@ -21,7 +24,7 @@ while True:
     
     # Extract all the profile names stored in the API response
     profiles += [project['profile']['name'] for project in data['results']]
-    
+
     # Grab the URL of the next page of results to fetch; if this entry is 
     # blank we have reached the last page
     if data["next"]:
