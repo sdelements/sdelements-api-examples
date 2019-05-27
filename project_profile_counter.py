56a7d0c05d45329f54387f4942ebc6f1515f7284
Athi Rajkumar <arajkumar@securitycompass.com>
2019-05-27 10:58:37 -0400

    Made some edits Ram suggested + some additional ones

--- project_profile_counter.py
+++ project_profile_counter.py
@@ -6,23 +6,23 @@ SERVER = 'https://tokyo.sdelements.com'
 API_TOKEN = 'f258c47557a3f98f55d4fbd0cb9d8354c86fbb52'
 PROJECTS_URL = '{server}/api/v2/projects/?page_size=100'.format(server=SERVER)
 REQUESTS_HEADER = {
-    'Authorization': "Token " + API_TOKEN,   # Authorization header + generated API TOKEN
+    # Authorization header + generated API TOKEN
+    'Authorization': "Token " + API_TOKEN,
     'Accept': "application/json"             # Accepts API response in JSON format
 }
-
 url = PROJECTS_URL
 profiles = []
+
 while True:
-    response = requests.get(url, headers=REQUESTS_HEADER) 
+    response = requests.get(url, headers=REQUESTS_HEADER)
     data = response.json()
-
-    for item in data['results']:    # loop through the values in the results key 
-        profiles.append(item['profile']['name'])    # add each profile's name to list
-
-    if data["next"]:    # check if a value is present in "next" 
+# Fetches all the profile names(including duplicates) stored in the API response
+    profiles += [project['profile']['name'] for project in data['results']]
+# Grab the URL of the next page of results to fetch; if this entry is blank we have reached the last page
+    if data["next"]:
         url = data["next"]
     else:
-        break   # leave the infinite loop if your on the last page
-
-for profile, count in collections.Counter(profiles).items():    # count the profile occurences  
+        break
+for profile, count in collections.Counter(profiles).items():
+# Counts the number of times each profile occurs in the list using Python's collection's module
     print(u"{} : {}".format(profile, count))
